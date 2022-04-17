77b2ac48af731b285cef69c9c6e9bc8d4f498f53
Sbastdia <sbastdia@myuax.com>
2022-04-17 14:12:42 +0200

    actualizado (usando a veces tabulador)

--- Servidor2.py
+++ Servidor2.py
@@ -8,9 +8,18 @@ miSocket.bind(('', 1234))
 miSocket.listen(5)
 
 while True:
+
     conexion, addr = miSocket.accept()
     print("Conexion establecida de ", addr)
 
+    peticion= conexion.recv(1024).decode()
+    print(peticion)
+
+    if peticion == "hola":
+        conexion.send("Adios".encode())
+    elif peticion == "adios":
+        conexion.send("Servidor cerrado".encode())
+        break
     conexion.send(b'Hola, soy el servidor')
     conexion.close()
 
